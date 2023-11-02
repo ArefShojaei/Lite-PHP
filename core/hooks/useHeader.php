a0895d86c4e0495f9d3530464fa3bfc6e1fa6fbd
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-02 20:13:15 +0330

    ADD: add useHeader hook

---
+++ core/hooks/useHeader.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @desc add header
+ * @function useHeader
+ * @param {string} $name - the header name
+ * @param {mixed} $defaultValue - the header value
+ * @return {void}
+ */
+function useHeader(string $name, mixed $defaultValue): void {
+    $_SERVER[$key] = $defaultValue;
+}
