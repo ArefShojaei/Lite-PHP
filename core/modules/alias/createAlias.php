fe50e322620d4df607fa8cbf48eb09de1e71f751
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-02 22:37:01 +0330

    ADD: add alias module

---
+++ core/modules/alias/createAlias.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @desc register alias
+ * @function createAlias
+ * @param {array} $aliases
+ * @return {void}
+ */
+function createAlias(array $aliases): void {
+    $GLOBALS['container']['aliases'] = [];
+    
+    foreach ($aliases as $alias => $path) {
+        $GLOBALS['container']['aliases'][$alias] = $path;
+    }
+}
