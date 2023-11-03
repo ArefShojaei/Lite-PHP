9176e66b0df110671b423e7bd3c38178fadb2f7b
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-03 17:15:54 +0330

    ADD: add useRequest hook

---
+++ core/hooks/useRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @desc get header value by name
+ * @function useRequest
+ * @param {string} $name
+ * @return {string}
+ */
+function useRequest(string $name): string {
+    function method(): string {
+        return $_SERVER["REQUEST_METHOD"];
+    }
+
+    function route(): string {
+        return $_SERVER["REQUEST_URI"];
+    }
+
+    function host(): string {
+        return $_SERVER["HTTP_HOST"];
+    }
+
+    function userAgent(): string {
+        return $_SERVER["HTTP_USER_AGENT"];
+    }
+
+    function protocol(): string {
+        return $_SERVER["REQUEST_SCHEME"];
+    }
+
+    function ip(): string {
+        return $_SERVER["REMOTE_ADDR"];
+    }
+
+    return $name();
+}
