56f06d0154b13ee8362a8e1fdc1e7bbc101d52c8
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-06 20:57:00 +0330

    ADD: add css helper

--- core/helpers/assets.php
+++ core/helpers/assets.php
@@ -9,3 +9,13 @@
 function asset(string $assetPath): string {
     return "public/{$assetPath}";
 }
+
+/**
+ * @desc get css path
+ * @function css
+ * @param {string} $filename
+ * @return {string}
+ */
+function css(string $filename): string {
+    return "public/css/{$filename}.css";
+}
