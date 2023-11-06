0bcc49370c87503ade2cf3ab486846973b0c3b43
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-06 21:02:22 +0330

    ADD: add host helper

--- core/helpers/url.php
+++ core/helpers/url.php
@@ -8,3 +8,12 @@
 function route(): string {
     return $_SERVER["REQUEST_URI"];
 }
+
+/**
+ * @desc current host
+ * @function host
+ * @return {string}
+ */
+function host(): string {
+    return $_SERVER["HTTP_HOST"];
+}
