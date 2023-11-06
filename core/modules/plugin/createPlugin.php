912c183aec00f8778452bf2d6443629128c42cf2
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-06 22:11:20 +0330

    UPDATE: update argument for adding plugin keyboard type

--- core/modules/plugin/createPlugin.php
+++ core/modules/plugin/createPlugin.php
@@ -5,8 +5,18 @@
  * @function createPlugin
  * @param {string} $name
  * @param {callback} $action
+ * @param {boolean} $hasUsageMode
  * @return {void}
  */
-function createPlugin(string $name, callable $action): void {
-    $GLOBALS["container"]["plugins"][$name] = $action();
+function createPlugin(string $name, callable $action, bool $hasUsageMode = true): void {
+    $keyboard = "helper";
+
+    if(!$usageMode) {
+        $keyboard = "runner";
+
+        $GLOBALS["container"]["plugins"][$keyboard][$name] = $action();
+        exit;
+    }
+    
+    $GLOBALS["container"]["plugins"][$keyboard][$name] = $action();
 }
