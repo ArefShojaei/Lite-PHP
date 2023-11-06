96c1404f1024281d1957658dcaaf9b41c657e4e1
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-06 21:13:03 +0330

    ADD: add import helper

---
+++ core/helpers/import.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @desc import file
+ * @function import
+ * @param {string} $path - alias + path 
+ * @param {string} $includeType 
+ * @return {void}
+ */
+function import(string $path, string $includeType = "include"): void {
+    $aliases = $GLOBALS['container']['aliases'];
+
+    $pattern = "/(?<alias>\@\w+)\b/";
+
+    preg_match($pattern, $path, $matches);
+
+    $alias = $matches['alias'];
+
+    $path = str_replace($alias, $aliases[$alias], $path);
+
+    $file = $path . ".php";
+
+    match($includeType) {
+        "include" => include $file,
+        "include_once" => include_once $file,
+        "require" => require $file,
+        "require_once" => require_once $file,
+    };
+}
