9a8e0698ba9c4f923fcf75071e65a0bb34f9460f
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-08 02:29:51 +0330

    UPDATE: update for using the useHTTP hook

--- core/hooks/useQuery.php
+++ core/hooks/useQuery.php
@@ -1,13 +1,19 @@
 <?php
 
 /**
+ * @package
+ */
+import("@core/hooks/useHTTP");
+
+
+/**
  * @desc route query string
  * @function useQuery
  * @param {string} $key
  * @return {array|string}
  */
 function useQuery(string $key = null): array|string {
-    $queries = $_SERVER['QUERY_STRING'];
+    $queries = useHTTP("QUERY_STRING");
 
     parse_str($queries, $parsedQueries);
 
