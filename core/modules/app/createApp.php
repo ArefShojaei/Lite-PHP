81fd35a208633cfef9469ce5788623e9adac99e5
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-08 17:31:39 +0330

    ADD: add the plugin helper

--- core/modules/app/createApp.php
+++ core/modules/app/createApp.php
@@ -9,6 +9,7 @@
 function createApp(callable $aciton): void {
     require_once "core/helpers/import.php";
     require_once "core/helpers/module.php";
+    require_once "core/helpers/plugin.php";
 
     $aciton();
 
