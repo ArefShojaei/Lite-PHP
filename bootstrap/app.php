7a5e5b91d5580946d471f0ade396bdbe95ff6d13
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-08 17:32:20 +0330

    ADD: add logger plugin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,5 +8,9 @@ import("@core/modules/module/createModule");
 
 # register all modules
 createModule(function() {
+    # plugins
+    registerPlugin("logger");
+    
+    # modules
     registerModule("app");
 });
