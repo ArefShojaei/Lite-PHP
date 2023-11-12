3dfa1cca71527e45f4352b606696d9f24f1bc55c
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-12 22:09:36 +0330

    ADD: add database module

--- core/modules/database/createDatabase.php
+++ core/modules/database/createDatabase.php
@@ -1,3 +1,14 @@
 <?php
 
-import("@core/hooks/useConfig");
+/**
+ * init database
+ * @function createDatabase
+ * @param string $host mysql host
+ * @param string $username mysql username
+ * @param string $passowrd mysql password
+ * @param string $db mysql database
+ * @return void
+ */
+function createDatabase(string $host, string $username, string $password, string $db): void {
+    $GLOBALS["mysql"] = mysqli_connect($host, $username, $password, $db); 
+}
