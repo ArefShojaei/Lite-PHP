4991e014743956c0e5d126ec81f612eac1cc119e
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-12 22:11:18 +0330

    ADD: add useQuery hook

---
+++ core/hooks/useQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * run mysql query
+ * @function useQuery
+ * @param string $query sql query
+ * @return string
+ */
+function useQuery(string $query): bool|array|null {
+    $connection = $GLOBALS["mysql"];
+
+    preg_match("/select|SELECT/", $query, $isMatch);
+    
+    $result = mysqli_query($connection, $query);
+
+    if((bool) $isMatch) {
+        return mysqli_fetch_all($result);
+    }
+
+    return true;
+}
