2752b5ad4cd756c85fe9d49cb0a21af8e85e0fa4
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-15 16:33:10 +0330

    ADD: add enum of app module

---
+++ modules/app/_enum.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package
+ */
+import("@core/modules/enum/createEnum");
+
+
+/**
+ * app enums
+ */
+createEnum("App", [
+    "NAME" => "Lite-PHP",
+    "WELCOME" => "PHP Freamework!"
+]);
