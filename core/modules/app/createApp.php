f1d1d316f7441784c91c4f63e269c7e97c26ccda
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-15 17:25:53 +0330

    ADD: add asset helpers

--- core/modules/app/createApp.php
+++ core/modules/app/createApp.php
@@ -10,6 +10,7 @@ function createApp(callable $aciton): void {
     require_once "core/helpers/import.php";
     require_once "core/helpers/module.php";
     require_once "core/helpers/plugin.php";
+    require_once "core/helpers/assets.php";
 
     $aciton();
 
