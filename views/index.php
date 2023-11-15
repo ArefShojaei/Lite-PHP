58157c432b569aca5bca750a5d0fd4206f695fff
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-15 17:23:46 +0330

    UPDATE: update content template

--- views/index.php
+++ views/index.php
@@ -5,8 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="<?php echo css('main'); ?>">
 </head>
 <body>
-    <h1><?php echo $message ?></h1>
+    <div id="container">
+        <h1>
+            Lite&nbsp; 
+            <span>PHP</span>&nbsp;
+            <img src="<?php echo img("php.svg"); ?>" alt="icon" width="60" height="60">
+        </h1>
+        <ul class="links">
+            <li><a href="https://github.com/ArefShojaei/Lite-PHP#categories-">Docs</a></li>
+            <li><a href="https://github.com/ArefShojaei">Author</a></li>
+            <li><a href="https://github.com/ArefShojaei/Lite-PHP">Repository</a></li>
+        </ul>
+    </div>
+
+    <script src="<?php echo js('app') ?>"></script>
 </body>
 </html>
