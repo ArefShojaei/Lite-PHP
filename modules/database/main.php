1f5410e78790f4604799718c248eb96d27d7a3cf
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-16 20:18:12 +0330

    ADD: add to init database

--- modules/database/main.php
+++ modules/database/main.php
@@ -5,3 +5,11 @@
  */
 import("@modules/database/_config");
 import("@core/modules/database/createDatabase");
+import("@core/hooks/useConfig");
+
+createDatabase(
+    useConfig("database.host"),
+    useConfig("database.username"),
+    useConfig("database.password"),
+    useConfig("database.db"),
+);
