bb8a89649300626bf38b5563bc58339f90ed419d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-16 16:59:02 +0330

    UPDATE: update plugin module

--- core/modules/plugin/createPlugin.php
+++ core/modules/plugin/createPlugin.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ * @package
+ */
+import("@core/hooks/useGlobal");
+
+
+/**
  * register plugin
  * @function createPlugin
  * @param string $name
@@ -9,14 +15,16 @@
  * @return void
  */
 function createPlugin(string $name, callable $action, bool $hasUsageMode = true): void {
+    $plugins = useGlobal("plugins");
+    
     $keyboard = "helper";
 
     if(!$hasUsageMode) {
         $keyboard = "runner";
 
-        $GLOBALS["container"]["plugins"][$keyboard][$name] = $action();
+        $plugins[$keyboard][$name] = $action();
         return;
     }
     
-    $GLOBALS["container"]["plugins"][$keyboard][$name] = $action();
+    $plugins[$keyboard][$name] = $action();
 }
