c7f61f82cdfc220c79c8fb73be673568f1133006
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-16 16:42:21 +0330

    UPDATE: update useBody hook

--- core/hooks/useBody.php
+++ core/hooks/useBody.php
@@ -3,11 +3,11 @@
 /**
  * request body data
  * @function useBody
- * @param string $key
+ * @param string|null $key
  * @return string|array
  */
-function useBody(string $key=null): string|array {
+function useBody(?string $key=null): string|array {
     $parsedBody = json_decode(file_get_contents("php://input"), true);
 
-    return $key ? $parsedBody[$key] : $parsedBody;
+    return isset($key) ? $parsedBody[$key] : $parsedBody;
 }
