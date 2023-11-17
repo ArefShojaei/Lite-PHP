8a1125aeee663ab2bdb1d79b88ccb3e200ac9c71
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-17 17:10:55 +0330

    UPDATE: update comment

--- core/hooks/useEnv.php
+++ core/hooks/useEnv.php
@@ -3,7 +3,7 @@
 /**
  * get variable by key from env container
  * @function useEnv
- * @param string} $key
+ * @param string $key
  * @return string|null
  */
 function useEnv(string $key): ?string {
