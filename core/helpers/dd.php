2357fd073ea755017581ea51ba0149825b644574
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-19 23:05:10 +0330

    ADD: add comment + the return data type

--- core/helpers/dd.php
+++ core/helpers/dd.php
@@ -1,6 +1,12 @@
 <?php
 
-function dd(array $input, $isDied = true) {
+/**
+ * dump and die an Array
+ * @param array $input
+ * @param bool $isDied
+ * @return void
+ */
+function dd(array $input, bool $isDied = true): void {
     echo "<pre>";
     print_r($input);
     
