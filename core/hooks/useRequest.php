86f3826eb289fe5d641e6d469cef4a369152f73d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-22 13:50:42 +0330

    UPDATE: update to add comments for logic code

--- core/hooks/useRequest.php
+++ core/hooks/useRequest.php
@@ -13,37 +13,70 @@ import("@core/hooks/useHTTP");
  * @return string
  */
 function useRequest(string $name): string {
+    /**
+     * request method
+     * @return string
+     */
     function method(): string {
         return useHTTP("REQUEST_METHOD");
     }
 
+    /**
+     * request url as "route"
+     * @return string
+     */
     function route(): string {
         return useHTTP("REQUEST_URI");
     }
 
+    /**
+     * server host
+     * @return string
+     */
     function host(): string {
         return useHTTP("HTTP_HOST");
     }
 
+    /**
+     * request client user-agent
+     * @return string
+     */
     function userAgent(): string {
         return useHTTP("HTTP_USER_AGENT");
     }
 
+    /**
+     * request protocol
+     * @return string
+     */
     function protocol(): string {
         return useHTTP("REQUEST_SCHEME");
     }
 
+    /**
+     * request client ip
+     * @return string
+     */
     function ip(): string {
         return useHTTP("REMOTE_ADDR");
     }
 
+    /**
+     * request route params
+     * @return array
+     */
     function params(): array {
         return useHTTP("params");
     }
 
+    /**
+     * request route param by key
+     * @return string
+     */
     function param(string $key): string {
         return useHTTP("params")[$key];
     }
+
 
     return $name();
 }
