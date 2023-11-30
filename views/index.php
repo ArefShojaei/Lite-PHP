e2e6ff514964971ad27db84dd6f4095f14719d63
ArefShojaei <arefshojaei82dev@gmail.com>
2023-11-30 16:14:26 +0330

    UPDATE: update template file

--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
         <h1>
             Lite&nbsp; 
             <span>PHP</span>&nbsp;
-            <img src="<?php echo img("php.svg"); ?>" alt="icon" width="60" height="60">
+            <img src="<?php echo icon("php.svg"); ?>" alt="icon" width="60" height="60">
         </h1>
         <ul class="links">
             <li><a href="https://github.com/ArefShojaei/Lite-PHP#categories-">Docs</a></li>
