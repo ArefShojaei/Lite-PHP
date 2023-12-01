91a72317891e4ea174af0a839603f59e6de201b6
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-01 14:52:36 +0330

    UPDATE: update plugin

--- plugins/security/main.php
+++ plugins/security/main.php
@@ -18,9 +18,7 @@ createPlugin("security", function() {
     useHeader("X-XSS-Protection", "0");
     useHeader("X-Content-Type-Options", "nosniff");
     useHeader("Content-Type", "text/html; charset=UTF-8");
-    useHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-    useHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline");
-    
+
     # REMOVE
     useHeader("X-Powered-By");
     useHeader("Server");
