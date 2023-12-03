89d667a345eb97f6e5c7745d7873a98cac059a32
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-03 16:22:12 +0330

    UPDATE: update to use the icon instead of img helper function

--- views/includes/header.php
+++ views/includes/header.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
     <link rel="stylesheet" href="<?php echo css('main'); ?>">
-    <link rel="shortcut icon" href="<?php echo img('php.svg'); ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo icon('php.svg'); ?>" type="image/x-icon">
 </head>
 <body>
