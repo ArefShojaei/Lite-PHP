8a2e710e03c6181c50f75ff4fb466576e8c71636
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-07 20:30:37 +0330

    ADD: add route and controller for '404 page'

--- modules/app/_controller.php
+++ modules/app/_controller.php
@@ -4,6 +4,7 @@
  * @package
  */
 import("@core/helpers/view");
+import("@core/helpers/abort");
 import("@core/hooks/useEnum");
 import("@core/hooks/useConfig");
 
@@ -17,4 +18,13 @@ function index(): void {
     view("welcome", [
         "title" => useConfig("app.name"),
     ]);
+}
+
+/**
+ * GET - show 404 page
+ * @function show404
+ * @return void
+ */
+function show404(): void {
+    abort(404, "Page Not Found!");
 }
--- modules/app/_route.php
+++ modules/app/_route.php
@@ -6,7 +6,9 @@
 import("@core/hooks/useRoute");
 import("@modules/app/_controller");
 
+
 /**
  * app routes
  */
 useRoute("GET", "/", "index");
+useRoute("GET", "/404", "show404");
