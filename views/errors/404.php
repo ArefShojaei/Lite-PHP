848bfc381f714ad9bdae4f74abb5ab7ffb99fcf8
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-07 20:36:24 +0330

    UPDATE: update to use dynamic values for 404 template

--- views/errors/404.php
+++ views/errors/404.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Page</title>
+    <title><?php echo $title ?> Page</title>
     <link rel="stylesheet" href="<?php echo css('main'); ?>">
     <link rel="shortcut icon" href="<?php echo img('php.svg'); ?>" type="image/x-icon">
 </head>
 <body>
-    <h1>Page Not Found!</h1>
+    <h1><?php echo $message ?></h1>
 </body>
 </html>
