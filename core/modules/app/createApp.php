50709433f4cc8ed557e6587c8e353bd48d157424
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-07 20:35:39 +0330

    UPDATE: update to init base helper functions

--- core/modules/app/createApp.php
+++ core/modules/app/createApp.php
@@ -13,8 +13,6 @@ session_start();
  */
 function createApp(callable $aciton): void {
     require_once "core/helpers/import.php";
-    require_once "core/helpers/module.php";
-    require_once "core/helpers/plugin.php";
     require_once "core/helpers/dd.php";
 
     $aciton();
