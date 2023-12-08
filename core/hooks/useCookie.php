a6d3147bcd0c13bc207dd22046cc08a123b41b76
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-08 18:46:01 +0330

    UPDATE: update to add comment

--- core/hooks/useCookie.php
+++ core/hooks/useCookie.php
@@ -8,19 +8,21 @@
  * @param array $params cookie options
  * @return mixed
  */
-function useCookie(string $key, mixed $defaultValue = null, array $params): mixed {
+function useCookie(string $key, mixed $defaultValue = null, array $params = []): mixed {
+    # get cookie
     if(!isset($defaultValue)) {
         return $_COOKIE[$key];
     }
 
+    # set cookie
     setcookie(
         $key, 
         $defaultValue,
         time() * $params["expireTime"],
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httpOnly"]
+        $params["path"] ?? "",
+        $params["domain"] ?? "",
+        $params["secure"] ?? false,
+        $params["httpOnly"] ?? false
     );
 
     return true;
