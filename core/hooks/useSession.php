cb3d046a129879f2763640d3ea76e6ff665c6c3f
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-08 18:50:31 +0330

    UPDATE: update to add comment

--- core/hooks/useSession.php
+++ core/hooks/useSession.php
@@ -8,12 +8,15 @@
  * @return mixed
  */
 function useSession(string $key, mixed $defaultValue = null): mixed {
+    # get session
     if(!isset($defaultValue)) {
         return $_SESSION[$key];
     }
 
+    # regenerate session ID
     session_regenerate_id(true);
 
+    # set session
     $_SESSION[$key] = $defaultValue;
 
     return true;
