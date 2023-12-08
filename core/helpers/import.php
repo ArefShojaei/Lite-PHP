9016daf2ea1b9548d1b2831d3686f08908bf5aae
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-08 15:06:31 +0330

    UPDATE: update to add comment

--- core/helpers/import.php
+++ core/helpers/import.php
@@ -8,18 +8,26 @@
  * @return void
  */
 function import(string $path, string $includeType = "require_once"): void {
+    # get aliases
     $aliases = $GLOBALS['container']['aliases'];
 
+    # decalre pattern
     $pattern = "/(?<alias>\@\w+)\b/";
 
+    # get matches for the pattern
     preg_match($pattern, $path, $matches);
 
+    # get the alias
     $alias = $matches['alias'];
 
+    # replace the alias with a "real path"
     $path = str_replace($alias, $aliases[$alias], $path);
 
-    $file = $path . ".php";
+    # get full path + file
+    $file = $path . PHP_FILE_EXTENTION;
 
+
+    # include the file by the $incldueType
     match($includeType) {
         "include" => include $file,
         "include_once" => include_once $file,
