d063d94c1bcf9b5aec0f34251349c468f8eb024c
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-08 16:13:26 +0330

    UPDATE: update to use the parse helper

--- core/hooks/useHTML.php
+++ core/hooks/useHTML.php
@@ -1,11 +1,17 @@
 <?php
 
 /**
+ * @package
+ */
+require_once "core/helpers/parse.php";
+
+
+/**
  * parse HTML by URL
  * @function useHTML
  * @param string $url
  * @return string|false
  */
 function useHTML(string $url): string|false {
-    return @file_get_contents($url);
+    return @parse($url);
 }
