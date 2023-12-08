23ba97f4dca9b4680ca09c70b748d903e13983bd
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-08 15:08:56 +0330

    ADD: add base constants

---
+++ core/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+# Base Path
+const BASE_ASSET_PATH = "/public";
+
+# File Extention
+const PHP_FILE_EXTENTION = ".php";
+const LOG_FILE_EXTENTION = ".log";
+const ENV_FILE_EXTENTION = ".env";
