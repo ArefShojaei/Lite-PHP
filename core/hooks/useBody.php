6dc8f49f6466f1242b21b3b1c6ce47fcb9e9fcbc
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-08 16:05:45 +0330

    UPDATE: update to add comment

--- core/hooks/useBody.php
+++ core/hooks/useBody.php
@@ -1,13 +1,19 @@
 <?php
 
 /**
+ * @package
+ */
+require_once "core/helpers/parse.php";
+
+
+/**
  * get request body data
  * @function useBody
- * @param null|string $key
+ * @param string $key
  * @return string|array
  */
-function useBody(?string $key = null): string|array {
-    $parsedBody = json_decode(file_get_contents("php://input"), true);
+function useBody(string $key = null): string|array {
+    $parsedBody = json_decode(parse("php://input"), true);
 
     return $key ? $parsedBody[$key] : $parsedBody;
 }
