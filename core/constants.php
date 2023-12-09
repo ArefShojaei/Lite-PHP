722f7f9628f205bfe6b92d5db4eaf9306a1c3426
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-09 21:46:28 +0330

    UPDATE: update to add flash constants

--- core/constants.php
+++ core/constants.php
@@ -7,3 +7,9 @@ const BASE_ASSET_PATH = "/public";
 const PHP_FILE_EXTENTION = ".php";
 const LOG_FILE_EXTENTION = ".log";
 const ENV_FILE_EXTENTION = ".env";
+
+# Flash Level
+const FLASH_INFO_LEVEL = "info";
+const FLASH_ERROR_LEVEL = "error";
+const FLASH_SUCCESS_LEVEL = "success";
+const FLASH_WARNING_LEVEL = "warning";
