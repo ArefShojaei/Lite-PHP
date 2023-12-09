3943e4bdd50cb59509678aecf3d0505fd75e7742
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-09 21:37:00 +0330

    REFACTOR: refactor logic code

--- core/hooks/useHash.php
+++ core/hooks/useHash.php
@@ -1,17 +1,11 @@
 <?php
 
 /**
- * bcrypt password
+ * hash password
  * @function useHash
  * @param string $password
  * @return array
  */
-function useHash(string $password): array {
-    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-    function verify($password, $hashedPassword): bool {
-        return password_verify($password, $hashedPassword);
-    }
-
-    return [$hashedPassword, "verify"];
+function useHash(string $password): string {
+    return password_hash($password, PASSWORD_BCRYPT);
 }
