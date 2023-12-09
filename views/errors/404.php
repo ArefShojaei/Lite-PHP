c35f42f10c1102bc0961f237a5a326951792ef24
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-09 22:16:42 +0330

    UPDATE: update template

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?> Page</title>
-    <link rel="stylesheet" href="<?php echo css('main'); ?>">
-    <link rel="shortcut icon" href="<?php echo img('php.svg'); ?>" type="image/x-icon">
-</head>
-<body>
+<?php include "includes/header.php" ?>
     <h1><?php echo $message ?></h1>
-</body>
-</html>
+<?php include "includes/footer.php" ?>
