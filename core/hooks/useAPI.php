785527259282a46699e95c18b8ddfe69bb0a6c8e
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-15 15:59:24 +0330

    REFACTOR: refactor logic code

--- core/hooks/useAPI.php
+++ core/hooks/useAPI.php
@@ -1,6 +1,59 @@
 <?php
 
 /**
+ * init curl as "open http request"
+ * @function _openHttpRequest
+ * @private
+ * @return object
+ */
+function _openHttpRequest(): object {
+    return curl_init();
+}
+
+/**
+ * set curl option
+ * @function _setHttpRequestOptions
+ * @private
+ * @param object $curl
+ * @param array $params
+ * @return void
+ */
+function _setHttpRequestOptions(object $curl, array $params): void {
+    # extract params
+    list($url, $method, $headers) = $params;
+
+    # set curl options
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+}
+
+/**
+ * send http request as "execute the curl"
+ * @function _sendHttpRequest
+ * @private
+ * @param object $curl
+ * @return string|bool
+ */
+function _sendHttpRequest(object $curl): string|bool {
+    return curl_exec($curl);
+}
+
+/**
+ * close http request as "close the curl"
+ * @function _closeHttpRequest
+ * @private
+ * @param object $curl
+ * @return void
+ */
+function _closeHttpRequest($curl): void {
+    curl_close($curl);
+}
+
+
+
+/**
  * send http request
  * @function useAPI
  * @param string $url
@@ -16,20 +69,47 @@ function useAPI(string $url, array $params = []): array {
 
 
     # init curl
-    $curl = curl_init();
+    $curl = _openHttpRequest();
  
     # set the curl config
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    _setHttpRequestOptions($curl, [$url, $method, $headers]);
 
     # get the curl response
-    $response = curl_exec($curl);
+    $response = _sendHttpRequest($curl);
 
     # close the curl
-    curl_close($curl);
+    _closeHttpRequest($curl);
 
-    # output response
+    # return response
     return json_decode($response, true);
 }
+
+
+
+
+// function useAPI(string $url, array $params = []): array {
+//     # get http method
+//     $method = $params['method'] ?? "GET";
+    
+//     # get http headers
+//     $headers = $params['headers'] ?? [];
+
+
+//     # init curl
+//     $curl = curl_init();
+ 
+//     # set the curl config
+//     curl_setopt($curl, CURLOPT_URL, $url);
+//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+//     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+//     # get the curl response
+//     $response = curl_exec($curl);
+
+//     # close the curl
+//     curl_close($curl);
+
+//     # output response
+//     return json_decode($response, true);
+// }
