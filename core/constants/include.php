8ca57a4203b0761a6536bda15f83f220612933c3
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-15 14:49:16 +0330

    ADD: add the include type constants

---
+++ core/constants/include.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ ** @summary incldue type constants
+ */
+
+
+/**
+ * include file type
+ * @constant string
+ * @return string
+ */
+const INCLUDE_TYPE = "include";
+
+/**
+ * include once file type
+ * @constant string
+ * @return string
+ */
+const INCLUDE_ONCE_TYPE = "include_once";
+
+/**
+ * require file type
+ * @constant string
+ * @return string
+ */
+const REQUIRE_TYPE = "require";
+
+/**
+ * require once file type
+ * @constant string
+ * @return string
+ */
+const REQUIRE_ONCE_TYPE = "require_once";
