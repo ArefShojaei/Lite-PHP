98e620c2f7386efb18fb01bbf9daa5381a3a010d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-15 15:35:25 +0330

    REFACTOR: refactor logic code | code structure

--- core/helpers/import.php
+++ core/helpers/import.php
@@ -1,37 +1,70 @@
 <?php
 
 /**
- * import file
- * @function import
- * @param string $path alias + path 
- * @param string $includeType
- * @return void
+ * @package
  */
-function import(string $path, string $includeType = "require_once"): void {
-    # get aliases
-    $aliases = $GLOBALS['container']['aliases'];
+require_once "core/hooks/useGlobal.php";
+require_once "core/hooks/useMatch.php";
 
+
+/**
+ * extract alias
+ * @function _extractAlias
+ * @private
+ * @param string $path
+ * @return string
+ */
+function _extractAlias(string $path): string {
     # decalre pattern
     $pattern = "/(?<alias>\@\w+)\b/";
 
     # get matches for the pattern
     preg_match($pattern, $path, $matches);
 
+    # return alias
+    return $matches["alias"];
+}
+
+/**
+ * replace alias to path
+ * @function _replaceAliasToPath
+ * @private
+ * @param array $aliases
+ * @param string $alias
+ * @param string $path
+ * @return string
+ */
+function _replaceAliasToPath(array $aliases, string $alias, string $path): string {
+    return str_replace($alias, $aliases[$alias], $path);
+}
+
+
+/**
+ * import file
+ * @function import
+ * @param string $path alias + path 
+ * @param string $includeType
+ * @return void
+ */
+function import(string $path, string $includeType = REQUIRE_ONCE_TYPE): void {
+    # get aliases
+    $aliases = useGlobal("aliases");
+
     # get the alias
-    $alias = $matches['alias'];
+    $alias = _extractAlias($path);
 
     # replace the alias with a "real path"
-    $path = str_replace($alias, $aliases[$alias], $path);
+    $path = _replaceAliasToPath($aliases, $alias, $path);
 
-    # get full path + file
-    $file = $path . PHP_FILE_EXTENTION;
+    # get file path
+    $filePath = $path . PHP_FILE_EXTENTION;
 
 
     # include the file by the $incldueType
     match($includeType) {
-        "include" => include $file,
-        "include_once" => include_once $file,
-        "require" => require $file,
-        "require_once" => require_once $file,
+        INCLUDE_TYPE => include $filePath,
+        INCLUDE_ONCE_TYPE => include_once $filePath,
+        REQUIRE_TYPE => require $filePath,
+        REQUIRE_ONCE_TYPE => require_once $filePath,
     };
 }
