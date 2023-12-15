45db693e1c9dc938dbb338393f19a48ecff67da4
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-15 15:36:10 +0330

    UPDATE: update comment

--- core/constants/index.php
+++ core/constants/index.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * use constants
+ * import constants
  */
 require_once "core/constants/path.php";
 require_once "core/constants/extention.php";
 require_once "core/constants/flash.php";
+require_once "core/constants/include.php";
