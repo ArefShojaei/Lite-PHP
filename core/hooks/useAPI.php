e40479f91f1aa8e89a13eca19ebc768b5b73bff4
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-15 16:03:49 +0330

    REFACTOR: refactor logic code

--- core/hooks/useAPI.php
+++ core/hooks/useAPI.php
@@ -83,33 +83,3 @@ function useAPI(string $url, array $params = []): array {
     # return response
     return json_decode($response, true);
 }
-
-
-
-
-// function useAPI(string $url, array $params = []): array {
-//     # get http method
-//     $method = $params['method'] ?? "GET";
-    
-//     # get http headers
-//     $headers = $params['headers'] ?? [];
-
-
-//     # init curl
-//     $curl = curl_init();
- 
-//     # set the curl config
-//     curl_setopt($curl, CURLOPT_URL, $url);
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-//     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-//     # get the curl response
-//     $response = curl_exec($curl);
-
-//     # close the curl
-//     curl_close($curl);
-
-//     # output response
-//     return json_decode($response, true);
-// }
