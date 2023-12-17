90d63694bc74523b99dfdd4f83eb7beec0bec884
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-17 16:25:53 +0330

    REFACTOR: refactor logic code

--- core/helpers/plugin.php
+++ core/helpers/plugin.php
@@ -1,11 +1,28 @@
 <?php
 
 /**
+ * @package
+ */
+import("@core/hooks/useGlobal");
+
+
+/**
  * register plugin
  * @function registerPlugin
  * @param string $plugin plugin name
+ * @param array $params plugin options
  * @return void
  */
-function registerPlugin(string $plugin): void {
+function registerPlugin(string $plugin, array $params = []): void {
+    # import plugin
     import("@plugins/{$plugin}/main");
+
+    # get plugins container
+    $plugins = useGlobal("plugins");
+
+    # get the plugin as "runner type"
+    $action = $plugins[PLUGIN_RUNNER_TYPE][$plugin];
+    
+    # call the plugin and pass input as $parmas
+    $action($params);
 }
