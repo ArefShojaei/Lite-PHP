9d79ad162b9005d27c55e39af3bc1a89e29fb5b8
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-17 22:24:12 +0330

    UPDATE: update to add constants

--- core/constants/index.php
+++ core/constants/index.php
@@ -8,3 +8,18 @@ require_once "core/constants/extention.php";
 require_once "core/constants/flash.php";
 require_once "core/constants/include.php";
 require_once "core/constants/plugin.php";
+
+
+/**
+ * main module filename
+ * @constant string
+ * @return string
+ */
+const MAIN_MODULE_FILE = "main";
+
+/**
+ * main plugin filename
+ * @constant string
+ * @return string
+ */
+const MAIN_PLUGIN_FILE = "main";
--- core/constants/path.php
+++ core/constants/path.php
@@ -6,8 +6,15 @@
 
 
 /**
- * asset path
+ * asset path as url for loading files
  * @constant string
  * @return string
  */
 const BASE_ASSET_PATH = "/public";
+
+/**
+ * views path
+ * @constant string
+ * @return string
+ */
+const BASE_VIEWS_PATH = "views";
