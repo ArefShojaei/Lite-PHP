1c7efcb7b8a0c861dcd64771410e163c099643da
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-17 16:28:24 +0330

    UPDATE: update to add default timezone & add plugin option

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,22 +8,30 @@ import("@core/helpers/plugin");
 import("@core/modules/module/createModule");
 import("@core/modules/router/createRouter");
 import("@core/hooks/useMode");
+import("@core/hooks/useEnv");
+
 
 
 /**
  * disable to show errors in "production mode"
  */
-if(useMode() !== 'development') error_reporting(0);
+if (useMode() !== "development") error_reporting(0);
+
+
+/**
+ * set default timezone for date
+ */
+date_default_timezone_set(useEnv("TIMEZONE"));
 
 
 /**
  * init modules
  */
-createModule(function() {
+createModule(function () {
     return [
         # list of plugins that has runner type
         "plugins" => [
-            registerPlugin("logger"),
+            registerPlugin("logger", ["level" => "short"]),
             registerPlugin("security"),
             registerPlugin("cors")
         ],
