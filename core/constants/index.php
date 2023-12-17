c84eac0a4904caba8f14a481cd653338b7f6f50d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-17 16:25:23 +0330

    ADD: add plugin constants

--- core/constants/index.php
+++ core/constants/index.php
@@ -7,3 +7,4 @@ require_once "core/constants/path.php";
 require_once "core/constants/extention.php";
 require_once "core/constants/flash.php";
 require_once "core/constants/include.php";
+require_once "core/constants/plugin.php";
---
+++ core/constants/plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ ** @summary plugin type constants
+ */
+
+
+/**
+ * plugin runner type
+ * @constant string
+ * @return string
+ */
+const PLUGIN_RUNNER_TYPE = "runner";
+
+/**
+ * plugin usage type
+ * @constant string
+ * @return string
+ */
+const PLUGIN_USAGE_TYPE = "usage";
