9568053cddcfde40ca099ef706db8c0d185c063d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-12-30 20:41:14 +0330

    UPDATE: update to use the __ helper as translate word

--- views/welcome.php
+++ views/welcome.php
@@ -3,12 +3,12 @@
         <h1>
             Lite&nbsp; 
             <span>PHP</span>&nbsp;
-            <img src="<?php echo icon("php.svg"); ?>" alt="icon" width="60" height="60">
+            <img src="<?php echo icon("php.svg"); ?>" alt="logo" width="60" height="60">
         </h1>
         <ul class="links">
-            <li><a href="https://github.com/ArefShojaei/Lite-PHP#categories-">Docs</a></li>
-            <li><a href="https://github.com/ArefShojaei">Author</a></li>
-            <li><a href="https://github.com/ArefShojaei/Lite-PHP">Repository</a></li>
+            <li><a href="https://github.com/ArefShojaei/Lite-PHP#categories-"><?php echo __("public.docs") ?></a></li>
+            <li><a href="https://github.com/ArefShojaei"><?php echo __("public.author") ?></a></li>
+            <li><a href="https://github.com/ArefShojaei/Lite-PHP"><?php echo __("public.repository") ?></a></li>
         </ul>
     </div>
 <?php include "includes/footer.php" ?>
