1771f80ebbe4ec4e9e08a6495a873ab40ab143f1
ArefShojaei <arefshojaei82dev@gmail.com>
2024-01-07 22:27:21 +0330

    ADD: add setup file

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package
+ */
+require_once "core/modules/app/createApp.php";
+require_once "core/modules/alias/createAlias.php";
+require_once "core/modules/env/createEnv.php";
+
+
+/**
+ * init app
+ */
+createApp(function() {
+    # register Aliases
+    createAlias([
+        "@core" => "core",
+        "@modules" => "modules",
+        "@plugins" => "plugins",
+        "@hooks" => "hooks",
+    ]);
+
+    # init Env
+    createEnv();
+});
