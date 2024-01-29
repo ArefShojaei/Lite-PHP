f75298f1da960ec49b1de44efd3ab235bc3febeb
ArefShojaei <arefshojaei82dev@gmail.com>
2024-01-29 22:06:38 +0330

    UPDATE: update to include all routes instead of _route file

--- modules/app/main.php
+++ modules/app/main.php
@@ -3,5 +3,13 @@
 /**
  * use modules
  */
+import("@core/hooks/useRoute");
 import("@modules/app/_config");
-import("@modules/app/_route");
+import("@modules/app/_controller");
+
+
+/**
+ * app routes
+ */
+useRoute("GET", "/", "index");
+useRoute("GET", "/404", "show404");
