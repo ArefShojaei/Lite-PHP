0ff8acd25c90d4b0966455eb3bdce6c930a48b9b
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-18 20:00:36 +0330

    REFACTOR: refactor logic code

--- core/modules/app/createApp.php
+++ core/modules/app/createApp.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * int session
+ * start session
  */
 session_start();
 
@@ -9,18 +9,8 @@ session_start();
 /**
  * init app
  * @function createApp
- * @param callable $aciton
  * @return void
  */
-function createApp(callable $aciton): void {
-    # init base files
-    require_once "core/constants/index.php";
-    require_once "core/helpers/import.php";
-    require_once "core/helpers/dd.php";
-
-    # call the $action as callback
-    $aciton();
-
-    # init bootstrap processes
+function createApp(): void {
     require_once "bootstrap/app.php";
 }
