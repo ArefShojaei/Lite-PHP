817c6571f61ad226d4622d3124128f42796977e5
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-18 20:01:08 +0330

    UPDATE: update logic code

--- public/index.php
+++ public/index.php
@@ -3,23 +3,11 @@
 /**
  * @package
  */
+require_once "core/loader.php";
 require_once "core/modules/app/createApp.php";
-require_once "core/modules/alias/createAlias.php";
-require_once "core/modules/env/createEnv.php";
 
 
 /**
  * bootstrap app
  */
-createApp(function() {
-    # int aliases
-    createAlias([
-        "@core" => "core",
-        "@modules" => "modules",
-        "@plugins" => "plugins",
-        "@hooks" => "hooks",
-    ]);
-
-    # init env
-    createEnv();
-});
+createApp();
