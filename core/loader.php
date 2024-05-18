9675407ee6504f70d15c3f579edd9a52f68c801d
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-18 19:59:25 +0330

    UPDATE: update to init files

--- core/loader.php
+++ core/loader.php
@@ -8,3 +8,11 @@
 require_once "core/constants/index.php";
 require_once "core/helpers/import.php";
 require_once "bootstrap/alias.php";
+require_once "core/helpers/dd.php";
+require_once "core/modules/env/createEnv.php";
+
+
+/**
+ * init Env
+ */
+createEnv();
