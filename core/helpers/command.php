c910fa919164eaf66487a602e8a084dbd41a4a50
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-19 18:19:44 +0330

    ADD: add command helper

---
+++ core/helpers/command.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package
+ */
+import("@core/hooks/useState");
+import("@core/hooks/useGlobal");
+
+
+
+/**
+ * add command
+ * @function addCommand
+ * @param string $command command name
+ * @param string|callable $action command action
+ * @param string $description command description
+ * @return void
+ */
+function addCommand(string $command, string|callable $action, string $description = ""): void {
+    $prefix = useGlobal("command-prefix");
+
+    useState("commands", [$prefix . $command], [
+        "action" => $action,
+        "description" => $description
+    ]);
+}
+
+
+/**
+ * add group command
+ * @function groupCommand
+ * @param string $command preifx command name
+ * @param callable $action bind commands action
+ * @return void
+ */
+function groupCommand(string $command, callable $aciton): void {
+    useState("command-prefix", [], $command);
+    
+    $aciton();
+    
+    useState("command-prefix", [], "");
+}
