c1151c9fe28dba7a26116f72cd6ee37a8a499847
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-22 22:27:19 +0330

    ADD: add test modules

---
+++ modules/app/_test.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package
+ */
+import("@core/helpers/test");
+import("@core/helpers/assert");
+
+
+/**
+ * app tests
+ */
+describe("App tests", function() {
+    it("should pass test", function() {
+        assertTrue(true);
+    });
+
+    it("should fail test", function() {
+        assertTrue(false);
+    });
+});
---
+++ modules/database/_test.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package
+ */
+import("@core/helpers/test");
+import("@core/helpers/assert");
+import("@core/hooks/useGlobal");
+import("@modules/database/main");
+
+
+/**
+ * database tests
+ */
+describe("Database Tests", function() {
+    it("should connect to the database", function() {
+        $connection = useGlobal("database");
+        
+        $result = isset($connection);
+
+        assertTrue($result);
+    });
+
+
+    it("should disconnect from the database", function() {
+        $connection = useGlobal("database");
+
+        $result = mysqli_close($connection);
+
+        assertTrue($result);
+    });
+});
