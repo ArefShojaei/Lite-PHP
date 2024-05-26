1abef3d093b5f42725efd010426e918be11c026a
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-26 12:09:19 +0330

    UPDATE: update to load css asset by filename

--- resources/views/includes/header.php
+++ resources/views/includes/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
-    <link rel="stylesheet" href="<?php echo css('main'); ?>">
+    <link rel="stylesheet" href="<?php echo css('styles'); ?>">
     <link rel="shortcut icon" href="<?php echo icon('php.svg'); ?>" type="image/x-icon">
 </head>
 <body>
