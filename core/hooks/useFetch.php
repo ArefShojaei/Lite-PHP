729a5ac4407ef1a7ade2decaab1ede0961468e05
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-29 00:22:37 +0330

    UPDATE: update to rename hook from 'useAPI' to 'useFetch'

--- core/hooks/useFetch.php
+++ core/hooks/useFetch.php
@@ -3,31 +3,34 @@
 /**
  * @package
  */
-import("@core/shared/hooks/useAPI/_openHttpRequest");
-import("@core/shared/hooks/useAPI/_closeHttpRequest");
-import("@core/shared/hooks/useAPI/_sendHttpRequest");
-import("@core/shared/hooks/useAPI/_setHttpRequestOptions");
+import("@core/shared/hooks/useFetch/_openHttpRequest");
+import("@core/shared/hooks/useFetch/_closeHttpRequest");
+import("@core/shared/hooks/useFetch/_sendHttpRequest");
+import("@core/shared/hooks/useFetch/_setHttpRequestOptions");
 
 
 /**
  * send http request
- * @function useAPI
+ * @function useFetch
  * @param string $url
  * @param array $params
  * @return array
  */
-function useAPI(string $url, array $params = []): array {
+function useFetch(string $url, array $params = []) {
     # get http method
     $method = $params['method'] ?? "GET";
     
     # get http headers
     $headers = $params['headers'] ?? [];
 
+    # get http body
+    $body = $params['body'] ?? [];
+
     # init curl
     $curl = _openHttpRequest();
     
     # set the curl config
-    _setHttpRequestOptions($curl, [$url, $method, $headers]);
+    _setHttpRequestOptions($curl, [$url, $method, $body, $headers]);
 
     # get the curl response
     $response = _sendHttpRequest($curl);
--- core/shared/hooks/useFetch/_setHttpRequestOptions.php
+++ core/shared/hooks/useFetch/_setHttpRequestOptions.php
@@ -10,11 +10,12 @@
  */
 function _setHttpRequestOptions(object $curl, array $params): void {
     # extract params
-    list($url, $method, $headers) = $params;
+    list($url, $method, $body, $headers) = $params;
 
     # set curl options
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 }
