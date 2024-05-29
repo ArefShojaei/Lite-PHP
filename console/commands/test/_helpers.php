fefac0aa5c94fb86d675f8b2d7f016ba68cc22ac
ArefShojaei <arefshojaei82dev@gmail.com>
2024-05-29 13:10:56 +0330

    UPDATE: update to add time-duration for test result

--- console/commands/test/_helpers.php
+++ console/commands/test/_helpers.php
@@ -16,13 +16,21 @@ function testAllModules(): void {
 
     $modules = getAllModules();
 
+    $startTime = microtime(true);
+
     # load all modules
     foreach ($modules as $module) {
         loadModule($module);
     }
 
+    $endTime = microtime(true);
+
+    $timeDuration = number_format(abs($startTime - $endTime), 2);
+
     # show result
-    echo "\r\nFinish Tests." . PHP_EOL;
+    echo "Finished Tests." . PHP_EOL;
+    echo "\r\nDuration: {$timeDuration} sec" . PHP_EOL;
+
 
     makeTable([$passedTests, $failedTests]);
 }
@@ -36,12 +44,21 @@ function testAllModules(): void {
  */
 function testModule($module): void {
     global $passedTests, $failedTests;
-    
+
+    $startTime = microtime(true);
+
     # load moudle by name
     loadModule($module);
 
+    $endTime = microtime(true);
+
+
+    $timeDuration = number_format(abs($startTime - $endTime), 2);
+
     # show result
-    echo "\r\nFinish Test." . PHP_EOL;
+    echo "Finished Test." . PHP_EOL;
+    echo "\r\nDuration: {$timeDuration} sec" . PHP_EOL;
+
 
     makeTable([$passedTests, $failedTests]);
 }
