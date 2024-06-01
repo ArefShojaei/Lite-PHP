db922e1a83a295aacaa4f95e748325d3e65ec88c
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-01 22:37:42 +0330

    UPDATE: update to add comment as marker for Logger plugin

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -11,12 +11,12 @@ import("@core/helpers/register");
  */
 return [
     "plugins" => [
-        registerPlugin("logger", ["level" => "short"]),
+        registerPlugin("logger", ["level" => "combined"]), # Log Level:  short | common | combined
         registerPlugin("security"),
         registerPlugin("cors")
     ],
     "modules" => [
         registerModule("database"),
-        registerModule("app")
+        registerModule("app"),
     ],
 ];
