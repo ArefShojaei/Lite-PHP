80d96d9981d03a4160b0f3bfda8fc00260e3e820
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-08 19:11:03 +0330

    UPDATE: update comment

--- modules/app/_controller.php
+++ modules/app/_controller.php
@@ -10,7 +10,9 @@ import("@core/hooks/useConfig");
 
 
 /**
- * GET - Show welcome page
+ * GET
+ * 
+ * Show welcome page
  * @function index
  * @return void
  */
@@ -21,7 +23,9 @@ function index(): void {
 }
 
 /**
- * GET - Show (404 | Not-found) page
+ * GET
+ * 
+ * Show (404 | Not-found) page
  * @function show404
  * @return void
  */
