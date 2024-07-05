e256429bc3d13b2c37c37089479adea163b3afaf
ArefShojaei <arefshojaei82dev@gmail.com>
2024-07-05 20:52:58 +0330

    REFACTOR: refactor logic code

--- core/hooks/useUpload.php
+++ core/hooks/useUpload.php
@@ -1,18 +1,38 @@
 <?php
 
 /**
+ * @package
+ */
+import("@core/shared/hooks/useUpload/_validateFileExtention");
+import("@core/shared/hooks/useUpload/_validateFileSize");
+
+
+/**
  * Upload file
  * @function useUpload
- * @param array $file
- * @return bool
+ * @param array $file file data
+ * @param array $ext list of valid file extentions
+ * @param float|int $size file size in 'MB'
+ * @return array
  */
-function useUpload(array $file): bool {
-    # Extract file info like name & ext
+function useUpload(array $file, array $ext = ['jpeg', 'jpg'], float|int $size = 2): array {
     $fileInfo = pathinfo($file['name']);
+    
+    // Validations
+    _validateFileExtention($fileInfo["extension"], $ext);
 
-    # Build dest path
+    _validateFileSize($file['size'], $size);
+
+
+    // Get full file path as dest path
     $distPath = buildPath(BASE_UPLOADS_PATH . "/", $fileInfo["filename"], "." . $fileInfo["extension"]);
+    
+    // Move to uploads folder by dist path
+    $status = move_uploaded_file($file["tmp_name"], $distPath);
 
-    # move the file
-    return move_uploaded_file($file["tmp_name"], $distPath);
+
+    return [
+        "status" => $status,
+        "message" => "File uploaded successfully"
+    ];
 }
