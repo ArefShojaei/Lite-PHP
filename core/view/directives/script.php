b9cf0c51c1c7a56cc2c343f271e787a9cf244d61
ArefShojaei <arefshojaei82dev@gmail.com>
2024-07-28 18:41:07 +0330

    ADD: add new script directives

--- core/view/directives/script.php
+++ core/view/directives/script.php
@@ -12,11 +12,20 @@ import("@core/helpers/directive");
  * @endscript
  * @php
  * @endphp
+ * @dump
+ * @dd
  */
 directive("linkStyle", fn(string $url): string => "<link rel='stylesheet' href='{$url}'>", true);
 directive("style", fn(): string => "<style>");
 directive("endstyle", fn(): string => "</style>");
 
 directive("linkScript", fn(string $url): string => "<script src='{$url}' defer>", true);
-directive("script", fn(): string => "<>");
+directive("script", fn(): string => "<script>");
 directive("endscript", fn(): string => "</script>");
+
+directive("php", fn(): string => "<?php");
+directive("endphp", fn(): string => "?>");
+
+directive("dump", fn(string $data): string => "<?php var_dump({$data}) ?>", true);
+
+directive("dd", fn(string $data): string => "<?php dd({$data}) ?>", true);
