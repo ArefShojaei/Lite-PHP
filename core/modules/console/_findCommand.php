c752e7852ae17a776ae5df31c6764e2be0f25afc
ArefShojaei <arefshojaei82dev@gmail.com>
2024-07-30 10:31:31 +0330

    UPDATE: update regex pattern

--- core/modules/console/_findCommand.php
+++ core/modules/console/_findCommand.php
@@ -15,8 +15,7 @@ function _findCommand(array $input): array {
 
     # Search to find exact command
     foreach ($commands as $command => $data) {
-        # Declare regex pattern
-        $pattern = "/^" . str_replace(["{", "}"], ["(?<", ">(\w|[-_])+)"], $command) . "$/";
+        $pattern = "/^" . str_replace(["{", "}"], ["(?<", ">(\w|[-_:\/.?=&])+)"], $command) . "$/";
 
         # Is matched comamnd with the regex
         preg_match($pattern, join(" ", $input), $result);
