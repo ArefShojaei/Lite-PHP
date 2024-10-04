92571be04fd97416e63538eebf94854a34643222
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-04 14:59:48 +0330

    ADD: add validator contracts

---
+++ core/contracts/validator/main.php
@@ -0,0 +1,50 @@
+<?php
+
+import("@core/modules/validator/createValidatorContract");
+
+
+/**
+ * Validator contracts
+ * 
+ * 
+ * ----------- Contracts -----------
+ ** required
+ ** type
+ ** min
+ ** max
+ * ---------------------------------
+ */
+createValidatorContract("required", function(string $schemaKey, mixed $schemaValue, string $validationType) use (&$reports) {
+    $result = [
+        "message" => "Value should not be null!",
+    ];
+    
+    if(empty($schemaValue)) $reports[$schemaKey]["required"] = $result;
+});
+
+createValidatorContract("type", function(string $schemaKey, mixed $schemaValue, string $validationType) use (&$reports) {
+    $result = [
+        "message" => "Type should be {$validationType}!",
+    ];
+    
+    $type = "is_{$validationType}"($schemaValue);
+
+
+    if(!$type) $reports[$schemaKey]["type"] = $result;
+});
+
+createValidatorContract("min", function(string $schemaKey, mixed $schemaValue, string $validationType) use (&$reports) {
+    $result = [
+        "message" => "Min Length should not be less than {$validationType}!",
+    ];
+
+    if(strlen($schemaValue) < $validationType) $reports[$schemaKey]["minLength"] = $result;
+});
+
+createValidatorContract("max", function(string $schemaKey, mixed $schemaValue, string $validationType) use (&$reports) {
+    $result = [
+        "message" => "Max Length should not be grater than {$validationType}!",
+    ];
+
+    if(strlen($schemaValue) > $validationType) $reports[$schemaKey]["maxLength"] = $result;
+});
