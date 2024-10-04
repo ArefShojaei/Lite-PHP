4cc9348636903299482f38dd687ae37f198f7e77
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-04 23:40:18 +0330

    FIX: fix bug to verify token

--- core/hooks/useVerifyToken.php
+++ core/hooks/useVerifyToken.php
@@ -6,12 +6,13 @@ import("@core/helpers/base64Url");
 function useVerifyToken(string $token, string $secretKey): array|bool|null {
     list($headerBase64Url, $payloadBase64Url, $signatureBase64Url) = explode(".", $token);
 
-    $header = json_encode(decodeBase64Url($headerBase64Url));
-    $payload = json_encode(decodeBase64Url($payloadBase64Url), true);
+    $header = json_decode(decodeBase64Url($headerBase64Url));
+    $payload = json_decode(decodeBase64Url($payloadBase64Url), true);
+
+
+    if(!$header || !$payload) return null;
     
-    if(!$header || $payload) return null;
-    
-    $signature = json_encode(decodeBase64Url($signatureBase64Url));
+    $signature = decodeBase64Url($signatureBase64Url);
     $expectedSignature = hash_hmac(JWT_ALG, "{$headerBase64Url}.{$payloadBase64Url}", $secretKey, true);
 
     if ($signature !== $expectedSignature) return null;
