253c718c6c1e1cfc22f120aac8f76c8c8e43fd5e
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-06 21:02:10 +0330

    REFACTOR: refactor logic code

--- core/hooks/useCache.php
+++ core/hooks/useCache.php
@@ -3,25 +3,47 @@
 import("@core/hooks/useError");
 import("@core/helpers/parse");
 import("@core/helpers/build");
+import("@core/hooks/useRecord");
+import("@core/hooks/useTable");
 
 
 /**
- * Cache data by file
+ * Get & Set cache data
  */
-function useCache(string $file, mixed $data = null): mixed {
-    $fileInfo = pathinfo($file);
-    $filename = $fileInfo["filename"];
-    $fileExt = "." . $fileInfo["extension"];
-    
-    $filePath = buildPath(CACHE_PATH . "/", $filename, $fileExt);
-    
-    # Get the cached data
-    if(!$data) {
-        !file_exists($filePath) && useError("\"{$file}\" file doesn't exist!");
+function useCache(string $name, array $data = null, int $expireTime = 3600): string|bool {
+    $filename = md5($name);
 
-        return parse($filePath);
-    }
+    $filePath = buildPath(CACHE_PATH . "/", $filename, JSON_FILE_EXTENTION);
+
     
-    # Set the cache data
-    return file_put_contents($filePath, $data);
+    # -------------- Timestamps --------------
+    $cachedTimestamp = useTable("timestamps", $name);
+    
+    $currentTimestamp = time();
+    
+    $futureTimestamp = $currentTimestamp + $expireTime;
+    # ----------------------------------------
+
+
+    # Delete the cached file & record
+    if (file_exists($filePath) && isset($cachedTimestamp) && $cachedTimestamp < $futureTimestamp) {
+        # Delete the file
+        unlink($filePath);
+
+        # Delete the cached timestamp data from local database
+        useTable("timestamps", $name, TABLE_DELETE_ACTION);
+
+        return true;
+    }
+
+    # Get the cached data  
+    if (!isset($data) || is_null($data)) return parse($filePath);
+
+
+    # Set the data
+    useRecord("timestamps", $name, $futureTimestamp);
+
+    file_put_contents($filePath, json_encode($data));
+
+    return true;
 }
