deac6ab71a615463f759cb1bcac1fdf167644d11
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-07 21:02:54 +0330

    UPDATE: update to add development state to render view

--- core/view/main.php
+++ core/view/main.php
@@ -3,6 +3,7 @@
 import("@core/shared/view/_loadDirectives");
 import("@core/view/compiler");
 import("@core/hooks/useTable");
+import("@core/hooks/useMode");
 
 
 function runViewEngine(): void {
@@ -13,13 +14,24 @@ function runViewEngine(): void {
     $currentTimestamp = time();
 
 
-    # Render view
+    # -------------------------------- #
+    #            States                #
+    # -------------------------------- #
+    
+    # Render view every time
+    if (useMode() !== PROD_MODE) {
+        compileView();
+
+        return;
+    }
+
+    # Render view at the first time
     if (!$lastViewTimestamp) {
         compileView();
 
         return;
     }
 
-    # Rerender view
+    # Rerender view after expired timestamp
     if ($lastViewTimestamp < $currentTimestamp) compileView();
 }
