d01f596274ec4b1d3cd3dce431cb41c1fe504992
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-07 19:00:36 +0330

    REFACTOR: refactor logic code

--- core/shared/view/_getTemplateFiles.php
+++ core/shared/view/_getTemplateFiles.php
@@ -1,8 +1,19 @@
 <?php
 
-function _getTemplateFiles(): array {
-    $rootTPL = glob(dirname(__DIR__, 3) . VIEWS_PATH . "/*" . VIEW_FILE_EXTENTION);
-    $subRootTPL = glob(dirname(__DIR__, 3) . VIEWS_PATH . "/*/*" . VIEW_FILE_EXTENTION);
+function _getTemplateFiles(int $maxDeepLevel = 7): array {
+    $files = [];
     
-    return [...$rootTPL, ...$subRootTPL];
+    $separator = "/*";
+
+
+    for ($count = 0; $count < $maxDeepLevel; $count++) {
+        $pattern = dirname(__DIR__, 3) . VIEWS_PATH . $separator . VIEW_FILE_EXTENTION;
+        
+        $separator .= $separator;
+
+        $files = array_merge($files, glob($pattern));
+    }
+
+    
+    return $files;
 }
