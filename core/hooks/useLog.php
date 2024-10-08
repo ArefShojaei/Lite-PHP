1f4a7414cd19ebdb0d06bb6916193e1da6a0aca4
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-08 23:02:12 +0330

    UPDATE: update logic code & comment message

--- core/hooks/useLog.php
+++ core/hooks/useLog.php
@@ -6,12 +6,10 @@ import("@core/helpers/build");
 /**
  * Add custom Log message
  */
-function useLog(string $message, string $filename = "app"): void {
+function useLog(string $message, string $level = LOG_INFO_LEVEL, string $filename = "app"): void {
     $filePath = buildPath(LOGS_PATH . "/", $filename, LOG_FILE_EXTENTION);
 
-    # Log content
-    $content = "[INFO] {$message}";
+    $content = "[{$level}] {$message}";
 
-    # append the log content to the file
     file_put_contents($filePath, $content . PHP_EOL, FILE_APPEND);
 }
