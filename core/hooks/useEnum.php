52f327b6c8d44f1cef5fa32d1e8c548cc9aa37ea
ArefShojaei <arefshojaei82dev@gmail.com>
2024-10-08 23:00:37 +0330

    UPDATE: update to add the return data type

--- core/hooks/useEnum.php
+++ core/hooks/useEnum.php
@@ -6,10 +6,10 @@ import("@core/hooks/useGlobal");
 /**
  * Get Enum by key
  */
-function useEnum(string $enum): string|int|array {
+function useEnum(string $enum): string|int|array|null {
     list($enum, $key) = explode("::", $enum);
 
     $enums = useGlobal("enums");
 
-    return $enums[$enum][$key];
+    return $enums[$enum][$key] ?? null;
 }
