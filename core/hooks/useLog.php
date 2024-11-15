9b01c1d025505affc9e38148d54f109d80603a1d
ArefShojaei <arefshojaei82dev@gmail.com>
2024-11-15 15:31:02 +0330

    UPDATE: update to add Log date content as custom message

--- core/hooks/useLog.php
+++ core/hooks/useLog.php
@@ -9,7 +9,9 @@ import("@core/helpers/build");
 function useLog(string $message, string $level = LOG_INFO_LEVEL, string $filename = "app"): void {
     $filePath = buildPath(LOGS_PATH . "/", $filename, LOG_FILE_EXTENTION);
 
-    $content = "[{$level}] {$message}";
+    $currentDate = date("Y/m/d H:m:sa"); 
+
+    $content = "[{$level}] {$currentDate} {$message}";
 
     file_put_contents($filePath, $content . PHP_EOL, FILE_APPEND);
 }
