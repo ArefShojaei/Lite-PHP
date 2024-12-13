577d43d4a07d5930eef7018445d32762cce00498
ArefShojaei <arefshojaei82dev@gmail.com>
2024-12-13 20:53:23 +0330

    REFACTOR: refactor logic code

--- core/hooks/useFlash.php
+++ core/hooks/useFlash.php
@@ -7,19 +7,21 @@ import("@core/hooks/useSession");
  * Get & Set Flash message
  */
 function useFlash(string $key, string $message = null, string $type = FLASH_INFO_LEVEL): array|bool {
-    $flash = useSession("flash");
-    
-    # Get the Flash
-    if(empty($message) && isset($flash[$key])) {
-        $message = $flash[$key];
+    $flashes = useSession("flashes") ?? [];
 
-        unset($flash[$key]);
+    # Get the flash
+    if (!isset($message)) {
+        $message = $flashes[$key] ?? null;
+    
+        unset($flashes[$key]);
+
+        useSession("flashes", $flashes);
 
         return [$message, $type];
     }
 
-    # Set the Falsh
-    $flash[$key] = $message;
+    # Set the flash;
+    $flashes[$key] = $message;
 
-    return true;
+    return useSession("flashes", $flashes);
 }
