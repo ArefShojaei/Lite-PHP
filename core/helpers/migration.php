91f70dfd2eebb754a0619c7420260e03a934f032
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-06 22:45:07 +0330

    UPDATE: update to rename migration helper names

--- core/helpers/migration.php
+++ core/helpers/migration.php
@@ -3,14 +3,14 @@
 import("@core/hooks/useQuery");
 
 
-function table_increment(string $table, string $column): void {
+function column_increment(string $table, string $column): void {
     useQuery("ALTER TABLE {$table} MODIFY COLUMN {$column} Integer");
 }
 
-function table_string(string $table, string $column, int $length = 255): void {
+function column_string(string $table, string $column, int $length = 255): void {
     useQuery("ALTER TABLE {$table} ADD COLUMN IF NOT EXISTS {$column} varchar($length)");
 }
 
-function table_integer(string $table, string $column, int $length): void {
+function column_integer(string $table, string $column, int $length): void {
     useQuery("ALTER TABLE {$table} ADD COLUMN IF NOT EXISTS {$column} int($length)");
 }
