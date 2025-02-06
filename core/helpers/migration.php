62b09c19c7970d9ae23c0937f23ef92cb07923e4
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-06 22:38:14 +0330

    FIX: fix bug for adding migration table column

--- core/helpers/migration.php
+++ core/helpers/migration.php
@@ -8,5 +8,5 @@ function table_increment(string $table, string $column): void {
 }
 
 function table_string(string $table, string $column, int $length = 255): void {
-    useQuery("ALTER TABLE {$table} ADD COLUMN {$column} varchar($length)");
+    useQuery("ALTER TABLE {$table} ADD COLUMN IF NOT EXISTS {$column} varchar($length)");
 }
