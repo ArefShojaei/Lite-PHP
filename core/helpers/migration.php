917f4ed152ddf786fdb68d347061ad7261e52bae
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-06 22:34:08 +0330

    ADD: add migration string feature helper

--- core/helpers/migration.php
+++ core/helpers/migration.php
@@ -6,3 +6,7 @@ import("@core/hooks/useQuery");
 function table_increment(string $table, string $column): void {
     useQuery("ALTER TABLE {$table} MODIFY COLUMN {$column} Integer");
 }
+
+function table_string(string $table, string $column, int $length = 255): void {
+    useQuery("ALTER TABLE {$table} ADD COLUMN {$column} varchar($length)");
+}
