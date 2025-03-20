60a190c0c498804b7967680699fe75a215d8130f
ArefShojaei <arefshojaei82dev@gmail.com>
2025-03-20 16:29:31 +0430

    UPDATE: update logic code

--- core/shared/hooks/useQuery/_escapeQuery.php
+++ core/shared/hooks/useQuery/_escapeQuery.php
@@ -1,13 +1,11 @@
 <?php
 
 function _escapeQuery(object $connection, string &$sql, array $params): void {
-    $sql = str_replace("?", "'?'", $sql);
+    $sql = str_replace("?", "%s", $sql);
 
     $scapedParams = array_map(function($param) use ($connection) {
         return mysqli_real_escape_string($connection, $param);
     }, $params);
 
-    $sql = preg_replace("/'\?'/", "%s", $sql);
-
-    $sql = vsprintf($sql, $scapedParams);    
+    $sql = vsprintf($sql, $scapedParams);
 }
