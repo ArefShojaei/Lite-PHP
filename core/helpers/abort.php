d552648e414424ca77b5dada5e220a2230c4d598
ArefShojaei <arefshojaei82dev@gmail.com>
2025-03-20 14:43:21 +0430

    UPDATE: update the return data type

--- core/helpers/abort.php
+++ core/helpers/abort.php
@@ -7,10 +7,10 @@ import("@core/helpers/view");
  * Throw HTTP error page
  * @param $data View data
  */
-function abort(int $code, string $message, $data = []): void {
+function abort(int $code, string $message, $data = []): string {
     # Set HTTP status code
     http_response_code($code);
 
     # Render template
-    view("errors.{$code}", array_merge(["title" => $code, "message" => $message], $data));
+    return view("errors.{$code}", array_merge(["title" => $code, "message" => $message], $data));
 }
