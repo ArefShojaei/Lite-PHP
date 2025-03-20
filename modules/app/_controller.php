422d747ca876c7d86a5edaf675d52bc87b208add
ArefShojaei <arefshojaei82dev@gmail.com>
2025-03-20 14:44:09 +0430

    ADD: add http error templates

--- modules/app/_controller.php
+++ modules/app/_controller.php
@@ -7,24 +7,37 @@ import("@core/hooks/useConfig");
 
 
 /**
- * GET - show welcome page
+ * App routes
  */
-function __app__index(): void {
-    view("welcome", [
+function __app__index(): string {
+    return view("welcome", [
         "title" => useConfig("app.name"),
     ]);
 }
 
 /**
- * GET - show 404 page
+ * HTTP error pages
  */
-function __app__404(): void {
-    abort(404, "Page Not Found!");
+function __app_500__index(): string {
+   return abort(500, useEnum("HTTP::500"));
 }
 
-/**
- * GET - show 500 page
- */
-function __app__500(): void {
-    abort(500, "Internal Server Error!");
+function __app_429__index(): string {
+   return abort(429, useEnum("HTTP::429"));
+}
+
+function __app_404__index(): string {
+   return abort(404, useEnum("HTTP::404"));
+}
+
+function __app_403__index(): string {
+   return abort(403, useEnum("HTTP::403"));
+}
+
+function __app_401__index(): string {
+   return abort(401, useEnum("HTTP::401"));
+}
+
+function __app_400__index(): string {
+   return abort(400, useEnum("HTTP::400"));
 }
--- modules/app/main.php
+++ modules/app/main.php
@@ -2,6 +2,7 @@
 
 import("@core/helpers/route");
 import("@modules/app/_config");
+import("@modules/app/_enum");
 import("@modules/app/_controller");
 
 
@@ -9,5 +10,9 @@ import("@modules/app/_controller");
  * Routes
  */
 addRoute("GET", "/", "__app__index");
-addRoute("GET", "/404", "__app__404");
-addRoute("GET", "/500", "__app__500");
+addRoute("GET", "/500", "__app_500__index");
+addRoute("GET", "/429", "__app_429__index");
+addRoute("GET", "/404", "__app_404__index");
+addRoute("GET", "/403", "__app_403__index");
+addRoute("GET", "/401", "__app_401__index");
+addRoute("GET", "/400", "__app_400__index");
