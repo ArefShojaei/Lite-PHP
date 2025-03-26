370f17b7757ba6f464583f79ddfb3424b9b48061
user <user@ex.com>
2025-03-26 17:29:03 +0330

    UPDATE: update logic code

--- core/hooks/useLog.php
+++ core/hooks/useLog.php
@@ -6,12 +6,12 @@ import("@core/helpers/build");
 /**
  * Add custom Log message
  */
-function useLog(string $message, string $level = LOG_INFO_LEVEL, string $filename = "app"): void {
+function useLog(string $message, string $filename = "app"): void {
     $filePath = buildPath(LOGS_PATH . "/", $filename, LOG_FILE_EXTENTION);
 
-    $currentDate = date("Y/m/d H:m:sa"); 
+    $currentDate = date("Y-m-d H:i:s"); 
 
-    $content = "[{$level}] {$currentDate} {$message}";
+    $content = "[{$currentDate}] {$message}";
 
     file_put_contents($filePath, $content . PHP_EOL, FILE_APPEND);
 }
