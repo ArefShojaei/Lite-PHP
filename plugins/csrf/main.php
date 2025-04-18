fe67cc18f5e0a5254ac6fc95a7f99c9cce268cb9
ArefShojaei <arefshojaei82dev@gmail.com>
2025-04-18 15:31:01 +0430

    FIX: fix CSRF-token logic code

--- plugins/csrf/main.php
+++ plugins/csrf/main.php
@@ -4,24 +4,23 @@ import("@core/modules/plugin/createPlugin");
 import("@core/hooks/useSession");
 import("@core/hooks/useHTTP");
 import("@core/hooks/usePOST");
+import("@core/helpers/abort");
 
 
 /**
  * @type Runner
  */
 createPlugin("csrf", function() {
-    $key = "csrf-token";
-
-    $token = useSession($key);
+    $key = "_token";
 
     # Set CSRF token
-    if (!isset($token) || empty($token)) {
+    if (!useSession($key)) {
         $token = bin2hex(random_bytes(32));
     
         useSession($key, $token);
     }
 
     # Validate CSRF token
-    if (useHTTP("REQUEST_METHOD") === "POST" && array_key_exists($key, usePOST()))
-        !hash_equals(useSession($key), usePOST($key)) && die("CSRF token is not valid!");
+    if (useHTTP("REQUEST_METHOD") === "POST") 
+        array_key_exists($key, usePOST()) && hash_equals(useSession($key), usePOST($key)) || die(abort(403, "Invalid CSRF token!"));
 }, false);
